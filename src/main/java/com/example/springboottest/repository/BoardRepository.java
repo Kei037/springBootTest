f020edfe921f2dc0b14f58b367af15a8b6d1d13a
Kei037 <juneway@hanmail.net>
2024-01-26 09:12:28 +0900

    Spring Boot Sample

--- src/main/java/com/example/springboottest/repository/BoardRepository.java
+++ src/main/java/com/example/springboottest/repository/BoardRepository.java
@@ -1,8 +1,11 @@
 package com.example.springboottest.repository;
 
 import com.example.springboottest.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
 }
